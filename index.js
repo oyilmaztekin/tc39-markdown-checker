bc665b969e0da333100fd46db7f583f365189efa
özer yilmaztekin <yilmaztekin.ozer@gmail.com>
2019-03-07 02:19:00 +0300

    completed createTemplate method

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const readMarkdown = require("./lib/readMarkdown");
 const parseToAST = require("./lib/parseToAst");
+const splitToStage = require("./lib/splitToStage");
 const generateTemplate = require("./lib/generateTemplate");
+const generateTable = require("./lib/createTable")
+  .generateTable;
 
-try {
-  const readme = readMarkdown.markdownFile();
-  const parsedFile = parseToAST(readme);
-  generateTemplate(parsedFile);
-} catch (error) {
-  throw Error(error);
-}
+const readme = readMarkdown.markdownFile();
+const parsedFile = parseToAST(readme);
+const hash = splitToStage(parsedFile.children);
+const template = generateTemplate(hash); //returns tuple
+const tableStage1 = generateTable(
+  hash["Stage_1"],
+  template[0]
+);
+
+// create a json file based on the constructed object
