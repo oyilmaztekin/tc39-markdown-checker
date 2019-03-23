0ee2f7dc0681ccee34fc010a05f3b5d4d580b0cc
Özer Yılmaztekin <yilmaztekin.ozer@gmail.com>
2019-03-23 15:06:37 +0300

    skipped header
    joined links for first column of first row

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const readMarkdown = require("./lib/readMarkdown");
 const parseToAST = require("./lib/parseToAst");
+const collectLinkDefinitions = require("./lib/collectLinkDefinitions");
 const splitToStage = require("./lib/splitToStage");
-const generateTemplate = require("./lib/generateTemplate");
 const generateTable = require("./lib/createTable")
   .generateTable;
 
 const readme = readMarkdown.markdownFile();
 const parsedFile = parseToAST(readme);
+const collectedLinkDefinitions = collectLinkDefinitions(parsedFile);
 const hash = splitToStage(parsedFile.children);
-const template = generateTemplate(hash); //returns tuple
 const tableStage1 = generateTable(
   hash["Stage_1"],
-  template[0]
+  "stage1",
+  collectedLinkDefinitions
 );
 
 // create a json file based on the constructed object
